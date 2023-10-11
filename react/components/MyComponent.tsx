ce35e8bb20d9130ea8fdf42b10d2663d0c9e9f7c
JuanEliasJabib02 <juaneliasjabib02@gmail.com>
2023-10-10 23:13:31 -0500

    add template

---
+++ react/components/MyComponent.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './my-component.css';
+
+const MyComponent: StorefrontFunctionComponent  = () => {
+  return (
+    <div className={styles.container__component}>My component</div>
+  );
+}
+
+export default MyComponent;
